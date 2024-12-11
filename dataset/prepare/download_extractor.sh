00502edfadf8334d6edc12797f78ffb7401cfb99
Seongeun Hong <37788658+seoneun@users.noreply.github.com>
2024-12-11 12:19:30 +0900

    Update download_extractor.sh

--- dataset/prepare/download_extractor.sh
+++ dataset/prepare/download_extractor.sh
@@ -1,6 +1,8 @@
 rm -rf checkpoints
 mkdir checkpoints
 cd checkpoints
+mkdir t2m
+cd t2m
 echo -e "Downloading extractors"
 
 gdown --fuzzy https://drive.google.com/file/d/1fiQgD4vQGk6q9ep4CEBWRi_SQAfalwjh/view?usp=sharing
